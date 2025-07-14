3abf625fbac2491fad15c4c7c7a2f50950105a21
Nicovolt <166955943+Nicovolt@users.noreply.github.com>
2025-07-14 17:57:23 -0300

    infoTransferencia

---
+++ src/main/java/servlets/ServletBuscarTitularCBU.java
@@ -0,0 +1,64 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import entidades.Cliente;
+import entidades.Cuenta;
+import negocio.ClienteNegocio;
+import negocio.CuentaNegocio;
+import negocioImpl.ClienteNegociolmpl;
+import negocioImpl.CuentaNegocioImpl;
+
+/**
+ * Servlet implementation class ServletBuscarTitularCBU
+ */
+@WebServlet("/ServletBuscarTitularCBU")
+public class ServletBuscarTitularCBU extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	private CuentaNegocio cuentaNegocio = new CuentaNegocioImpl();
+    private ClienteNegocio clienteNegocio = new ClienteNegociolmpl();
+    
+    
+public ServletBuscarTitularCBU() {
+  
+}
+
+
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	response.getWriter().append("Served at: ").append(request.getContextPath());
+}
+
+
+protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	 String cbu = request.getParameter("cbu");
+
+	    response.setContentType("application/json");
+	    response.setCharacterEncoding("UTF-8");
+
+	    PrintWriter out = response.getWriter(); // ← AÑADÍ ESTA LÍNEA
+
+	    if (cuentaNegocio.existeCBU(cbu)) {
+	        Cuenta cuenta = cuentaNegocio.buscarIdConCbu(cbu);
+	        Cliente cliente = clienteNegocio.BuscarPorID(cuenta.getIdCliente());
+
+	        String json = "{ \"nombre\": \"" + cliente.getNombre() + "\", " +
+	                      "\"apellido\": \"" + cliente.getApellido() + "\", " +
+	                      "\"dni\": \"" + cliente.getDni() + "\" }";
+
+	        out.print(json);
+	    } else {
+	        out.print("{ \"error\": \"CBU no encontrado\" }");
+	    }
+
+	    out.flush();
+	}
+}
